4c34320d5265931a1fed229e05a383100407a0a9
anand <anandpromax@gmail.com>
2024-02-19 18:33:14 +0530

    Refactor code to improve performance and readability

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+const db = require("./models/index");
+const app = express();
+
+const setUpAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use("/api", apiRoutes);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.listen(PORT, () => {
+    console.log(`Server is running on port  ${PORT}`);
+    if (process.env.DB) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
+
+setUpAndStartServer();
